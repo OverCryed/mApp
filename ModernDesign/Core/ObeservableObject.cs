55116d239160c874f8d6991d09ce91ae97c6b65c
Matus Laurinec <matus.laurinec@mts.sk>
2021-08-24 11:54:46 +0200

    Add project files.

---
+++ ModernDesign/Core/ObeservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernDesign.Core
+{
+    class ObeservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
