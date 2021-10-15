f3159469dda0292c87c084551e492dde79011114
LAPTOP-D09TLMC3\ASUS <laurinecmatus@gmail.com>
2021-10-15 19:55:44 +0200

    init

--- ModernDesign/MVVM/ViewModel/MainViewModel.cs
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -9,7 +9,7 @@ namespace ModernDesign.MVVM.ViewModel
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand DiscoveryCommand { get; set; }
         public HomeViewModel HomeVM { get; set; }
-        public DiscoveryViewModel DiscoveryVM { get; set; }
+
 
         private object _currentView;
 
@@ -26,18 +26,12 @@ namespace ModernDesign.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
-            DiscoveryVM = new DiscoveryViewModel();
 
             CurrentView = HomeVM;
 
             HomeViewCommand = new RelayCommand(o =>
             {
                 CurrentView = HomeVM;
-            });
-
-            DiscoveryCommand = new RelayCommand(o =>
-            {
-                CurrentView = DiscoveryVM;
             });
         }
     }
