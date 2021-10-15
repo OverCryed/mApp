c3b1d2b3210df8aaf360bbaee4c446eed7c7688a
LAPTOP-D09TLMC3\ASUS <laurinecmatus@gmail.com>
2021-10-15 22:49:50 +0200

    vizualne upravy

--- ModernDesign/MainWindow.xaml.cs
+++ ModernDesign/MainWindow.xaml.cs
@@ -13,12 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ModernDesign
+namespace ModernDesign 
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
         public MainWindow()
         {
